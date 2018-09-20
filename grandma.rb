ce60fe1a8bec3a9c6374e7096386f6a44740b2f3
embeor <embeor@gmail.com>
2018-09-20 20:48:30 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,7 @@ require "pry"
 def speak_to_grandma (phrase)
 #binding.pry
   if phrase == "I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU TOO PUMPKIN!"
+    "I LOVE YOU TOO PUMPKIN!"
   end
 end
 # Whatever you say to grandma, she should respond with
